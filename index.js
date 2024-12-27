7c665bd3aa5dcda5b08b35a1e40e9f8b80ca3329
sailendrachettri <sailendra9083@gmail.com>
2024-12-27 21:58:50 +0530

    bug fixes

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ export default function getGreeting() {
     const currentHour = new Date().getHours();
 
     if (currentHour >= 0 && currentHour < 12) {
-        return "Good Morning,";
+        return "Good Morning";
     } else if (currentHour === 12) {
-        return "Good Noon,";
+        return "Good Noon";
     } else if (currentHour > 12 && currentHour < 18) {
-        return "Good Afternoon,";
+        return "Good Afternoon";
     } else {
-        return "Good Evening,";
+        return "Good Evening";
     }
 };
